091c8531bdd839a8eec6f88eb7e68be4f8e131cf
Derek Hageman <Derek.Hageman@noaa.gov>
2021-08-02 10:11:44 -0600

    Restrict editing examples to example mode
    Make sure the primary mode is for examples rather than allowing anything
    else for the station.

--- forge/vis/editing/permissions.py
+++ forge/vis/editing/permissions.py
@@ -1,12 +1,13 @@
 from starlette.requests import Request
-from forge.vis.mode.permissions import is_available as mode_available
 
 
 def is_available(request: Request, station: str, mode_name: str):
-    return mode_available(request, station, mode_name)
+    if mode_name == "example-editing":
+        return True
+    return request.user.allow_mode(station, mode_name)
 
 
 def is_writable(request: Request, station: str, mode_name: str):
-    if mode_name.startswith("example-"):
+    if mode_name == "example-editing":
         return True
     return request.user.allow_mode(station, mode_name, write=True)
