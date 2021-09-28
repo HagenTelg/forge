af200cc2e34c484126797b9da4621c8746483d99
Derek Hageman <Derek.Hageman@noaa.gov>
2021-09-28 09:31:42 -0600

    Add BEO station plots configuration
    Add configuration for BEO plots.

---
+++ forge/vis/station/beo/mode.py
@@ -0,0 +1,26 @@
+import typing
+from ..default.mode import Mode, detach, aerosol_modes
+
+
+station_modes = detach(aerosol_modes)
+
+
+station_modes['aerosol-raw'].remove('aerosol-raw-counts')
+station_modes['aerosol-editing'].remove('aerosol-editing-counts')
+station_modes['aerosol-clean'].remove('aerosol-clean-counts')
+station_modes['aerosol-avgh'].remove('aerosol-avgh-counts')
+station_modes['aerosol-raw'].remove('aerosol-raw-cpcstatus')
+
+station_modes['aerosol-raw'].remove('aerosol-raw-aethalometer')
+station_modes['aerosol-raw'].remove('aerosol-raw-aethalometerstatus')
+station_modes['aerosol-editing'].remove('aerosol-editing-aethalometer')
+station_modes['aerosol-editing'].remove('aerosol-editing-aethalometerstatus')
+station_modes['aerosol-clean'].remove('aerosol-clean-aethalometer')
+station_modes['aerosol-avgh'].remove('aerosol-avgh-aethalometer')
+
+station_modes['aerosol-raw'].remove('aerosol-raw-flow')
+station_modes['aerosol-raw'].remove('aerosol-raw-umacstatus')
+
+
+def get(station: str, mode_name: str) -> typing.Optional[Mode]:
+    return station_modes.get(mode_name)
