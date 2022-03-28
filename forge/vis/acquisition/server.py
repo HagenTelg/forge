fe78f2efca197ca4fd8a63b33efb78889c32cf56
Derek Hageman <Derek.Hageman@noaa.gov>
2022-03-28 12:32:51 -0600

    Always allow acquisition example sockets
    Do not filter the mode for the acquisition example socket, instead
    just filter the station.  This makes it act more like the data socket
    for allowing example data for any permitted station.

--- forge/vis/acquisition/server.py
+++ forge/vis/acquisition/server.py
@@ -300,7 +300,7 @@ class _ExampleSocket(WebSocketEndpoint):
         self.station = websocket.path_params['station'].lower()
         if self.station not in STATIONS:
             raise HTTPException(starlette.status.HTTP_404_NOT_FOUND, detail="Invalid station")
-        if not websocket.user.allow_mode(self.station, 'acquisition'):
+        if not websocket.user.allow_station(self.station):
             raise HTTPException(starlette.status.HTTP_403_FORBIDDEN, detail="Station not available")
 
         await websocket.accept()
