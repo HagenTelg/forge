e224040271e4941f9e52fc200221563778392b43
Derek Hageman <Derek.Hageman@noaa.gov>
2023-02-08 07:41:13 -0700

    UGR aethalometer to A41
    Change UGR plotted aethalometer to A41 from A42.

--- forge/vis/station/ugr/data.py
+++ forge/vis/station/ugr/data.py
@@ -60,38 +60,38 @@ station_profile_data['aerosol']['raw']['aethalometerstatus'] = lambda station, s
     }, send
 )
 station_profile_data['aerosol']['realtime']['aethalometer'] = dict(
-    [(RealtimeTranslator.Key(f'Ba{i+1}_A42'), f'Ba{i+1}') for i in range(7)] +
-    [(RealtimeTranslator.Key(f'X{i+1}_A42'), f'X{i+1}') for i in range(7)] +
-    [(RealtimeTranslator.Key(f'ZFACTOR{i+1}_A42'), f'CF{i+1}') for i in range(7)] +
-    [(RealtimeTranslator.Key(f'Ir{i+1}_A42'), f'Ir{i+1}') for i in range(7)]
+    [(RealtimeTranslator.Key(f'Ba{i+1}_A41'), f'Ba{i+1}') for i in range(7)] +
+    [(RealtimeTranslator.Key(f'X{i+1}_A41'), f'X{i+1}') for i in range(7)] +
+    [(RealtimeTranslator.Key(f'ZFACTOR{i+1}_A41'), f'CF{i+1}') for i in range(7)] +
+    [(RealtimeTranslator.Key(f'Ir{i+1}_A41'), f'Ir{i+1}') for i in range(7)]
 )
 station_profile_data['aerosol']['realtime']['aethalometerstatus'] = {
-    RealtimeTranslator.Key('T1_A42'): 'Tcontroller',
-    RealtimeTranslator.Key('T2_A42'): 'Tsupply',
-    RealtimeTranslator.Key('T3_A42'): 'Tled',
+    RealtimeTranslator.Key('T1_A41'): 'Tcontroller',
+    RealtimeTranslator.Key('T2_A41'): 'Tsupply',
+    RealtimeTranslator.Key('T3_A41'): 'Tled',
 }
 station_profile_data['aerosol']['clean']['aethalometer'] = lambda station, start_epoch_ms, end_epoch_ms, send: DataReader(
     start_epoch_ms, end_epoch_ms, dict(
-        [(Name(station, 'clean', f'Ba{i+1}_A42'), f'Ba{i+1}') for i in range(7)] +
-        [(Name(station, 'clean', f'X{i+1}_A42'), f'X{i+1}') for i in range(7)] +
-        [(Name(station, 'clean', f'ZFACTOR{i+1}_A42'), f'CF{i+1}') for i in range(7)] +
-        [(Name(station, 'clean', f'Ir{i+1}_A42'), f'Ir{i+1}') for i in range(7)]
+        [(Name(station, 'clean', f'Ba{i+1}_A41'), f'Ba{i+1}') for i in range(7)] +
+        [(Name(station, 'clean', f'X{i+1}_A41'), f'X{i+1}') for i in range(7)] +
+        [(Name(station, 'clean', f'ZFACTOR{i+1}_A41'), f'CF{i+1}') for i in range(7)] +
+        [(Name(station, 'clean', f'Ir{i+1}_A41'), f'Ir{i+1}') for i in range(7)]
     ), send
 )
 station_profile_data['aerosol']['avgh']['aethalometer'] = lambda station, start_epoch_ms, end_epoch_ms, send: DataReader(
     start_epoch_ms, end_epoch_ms, dict(
-        [(Name(station, 'avgh', f'Ba{i+1}_A42'), f'Ba{i+1}') for i in range(7)] +
-        [(Name(station, 'avgh', f'X{i+1}_A42'), f'X{i+1}') for i in range(7)] +
-        [(Name(station, 'avgh', f'ZFACTOR{i+1}_A42'), f'CF{i+1}') for i in range(7)] +
-        [(Name(station, 'avgh', f'Ir{i+1}_A42'), f'Ir{i+1}') for i in range(7)]
+        [(Name(station, 'avgh', f'Ba{i+1}_A41'), f'Ba{i+1}') for i in range(7)] +
+        [(Name(station, 'avgh', f'X{i+1}_A41'), f'X{i+1}') for i in range(7)] +
+        [(Name(station, 'avgh', f'ZFACTOR{i+1}_A41'), f'CF{i+1}') for i in range(7)] +
+        [(Name(station, 'avgh', f'Ir{i+1}_A41'), f'Ir{i+1}') for i in range(7)]
     ), send
 )
 station_profile_data['aerosol']['editing']['aethalometer'] = lambda station, start_epoch_ms, end_epoch_ms, send: EditedReader(
     start_epoch_ms, end_epoch_ms, station, 'aerosol', dict(
-        [(Name(station, 'clean', f'Ba{i + 1}_A42'), f'Ba{i + 1}') for i in range(7)] +
-        [(Name(station, 'clean', f'X{i + 1}_A42'), f'X{i + 1}') for i in range(7)] +
-        [(Name(station, 'clean', f'ZFACTOR{i + 1}_A42'), f'CF{i + 1}') for i in range(7)] +
-        [(Name(station, 'clean', f'Ir{i + 1}_A42'), f'Ir{i + 1}') for i in range(7)]
+        [(Name(station, 'clean', f'Ba{i + 1}_A41'), f'Ba{i + 1}') for i in range(7)] +
+        [(Name(station, 'clean', f'X{i + 1}_A41'), f'X{i + 1}') for i in range(7)] +
+        [(Name(station, 'clean', f'ZFACTOR{i + 1}_A41'), f'CF{i + 1}') for i in range(7)] +
+        [(Name(station, 'clean', f'Ir{i + 1}_A41'), f'Ir{i + 1}') for i in range(7)]
     ), send
 )
 
