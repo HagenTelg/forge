00e7763cd16ead1524f513b8a702d802fa55255f
Derek Hageman <Derek.Hageman@noaa.gov>
2023-02-09 12:48:24 -0700

    Fix SNS aethalometer
    SNS aethalometer is A42.

---
+++ forge/vis/station/sns/data.py
@@ -0,0 +1,62 @@
+import typing
+from ..cpd3 import DataStream, DataReader, RealtimeTranslator, Name, data_profile_get, detach, profile_data
+
+
+station_profile_data = detach(profile_data)
+
+
+station_profile_data['aerosol']['raw']['aethalometer'] = lambda station, start_epoch_ms, end_epoch_ms, send: DataReader(
+    start_epoch_ms, end_epoch_ms, dict(
+        [(Name(station, 'raw', f'Ba{i+1}_A42'), f'Ba{i+1}') for i in range(7)] +
+        [(Name(station, 'raw', f'X{i+1}_A42'), f'X{i+1}') for i in range(7)] +
+        [(Name(station, 'raw', f'ZFACTOR{i+1}_A42'), f'CF{i+1}') for i in range(7)] +
+        [(Name(station, 'raw', f'Ir{i+1}_A42'), f'Ir{i+1}') for i in range(7)]
+    ), send
+)
+station_profile_data['aerosol']['raw']['aethalometerstatus'] = lambda station, start_epoch_ms, end_epoch_ms, send: DataReader(
+    start_epoch_ms, end_epoch_ms, {
+        Name(station, 'raw', 'T1_A42'): 'Tcontroller',
+        Name(station, 'raw', 'T2_A42'): 'Tsupply',
+        Name(station, 'raw', 'T3_A42'): 'Tled',
+    }, send
+)
+station_profile_data['aerosol']['realtime']['aethalometer'] = dict(
+    [(RealtimeTranslator.Key(f'Ba{i+1}_A42'), f'Ba{i+1}') for i in range(7)] +
+    [(RealtimeTranslator.Key(f'X{i+1}_A42'), f'X{i+1}') for i in range(7)] +
+    [(RealtimeTranslator.Key(f'ZFACTOR{i+1}_A42'), f'CF{i+1}') for i in range(7)] +
+    [(RealtimeTranslator.Key(f'Ir{i+1}_A42'), f'Ir{i+1}') for i in range(7)]
+)
+station_profile_data['aerosol']['realtime']['aethalometerstatus'] = {
+    RealtimeTranslator.Key('T1_A42'): 'Tcontroller',
+    RealtimeTranslator.Key('T2_A42'): 'Tsupply',
+    RealtimeTranslator.Key('T3_A42'): 'Tled',
+}
+station_profile_data['aerosol']['clean']['aethalometer'] = lambda station, start_epoch_ms, end_epoch_ms, send: DataReader(
+    start_epoch_ms, end_epoch_ms, dict(
+        [(Name(station, 'clean', f'Ba{i+1}_A42'), f'Ba{i+1}') for i in range(7)] +
+        [(Name(station, 'clean', f'X{i+1}_A42'), f'X{i+1}') for i in range(7)] +
+        [(Name(station, 'clean', f'ZFACTOR{i+1}_A42'), f'CF{i+1}') for i in range(7)] +
+        [(Name(station, 'clean', f'Ir{i+1}_A42'), f'Ir{i+1}') for i in range(7)]
+    ), send
+)
+station_profile_data['aerosol']['avgh']['aethalometer'] = lambda station, start_epoch_ms, end_epoch_ms, send: DataReader(
+    start_epoch_ms, end_epoch_ms, dict(
+        [(Name(station, 'avgh', f'Ba{i+1}_A42'), f'Ba{i+1}') for i in range(7)] +
+        [(Name(station, 'avgh', f'X{i+1}_A42'), f'X{i+1}') for i in range(7)] +
+        [(Name(station, 'avgh', f'ZFACTOR{i+1}_A42'), f'CF{i+1}') for i in range(7)] +
+        [(Name(station, 'avgh', f'Ir{i+1}_A42'), f'Ir{i+1}') for i in range(7)]
+    ), send
+)
+station_profile_data['aerosol']['editing']['aethalometer'] = lambda station, start_epoch_ms, end_epoch_ms, send: EditedReader(
+    start_epoch_ms, end_epoch_ms, station, 'aerosol', dict(
+        [(Name(station, 'clean', f'Ba{i + 1}_A42'), f'Ba{i + 1}') for i in range(7)] +
+        [(Name(station, 'clean', f'X{i + 1}_A42'), f'X{i + 1}') for i in range(7)] +
+        [(Name(station, 'clean', f'ZFACTOR{i + 1}_A42'), f'CF{i + 1}') for i in range(7)] +
+        [(Name(station, 'clean', f'Ir{i + 1}_A42'), f'Ir{i + 1}') for i in range(7)]
+    ), send
+)
+
+
+def get(station: str, data_name: str, start_epoch_ms: int, end_epoch_ms: int,
+        send: typing.Callable[[typing.Dict], typing.Awaitable[None]]) -> typing.Optional[DataStream]:
+    return data_profile_get(station, data_name, start_epoch_ms, end_epoch_ms, send, station_profile_data)
---
+++ forge/vis/station/sns/export.py
@@ -0,0 +1,41 @@
+import typing
+from ..cpd3 import Export, ExportList, DataExportList, DataExport, Name, export_profile_get, export_profile_lookup, detach, profile_export
+
+
+station_profile_export = detach(profile_export)
+
+
+station_profile_export['aerosol']['raw']['aethalometer'].data = lambda station, start_epoch_ms, end_epoch_ms, directory: DataExport(
+    start_epoch_ms, end_epoch_ms, directory, 'unsplit', set(
+        [Name(station, 'raw', f'Ba{i + 1}_A42') for i in range(7)] +
+        [Name(station, 'raw', f'X{i + 1}_A42') for i in range(7)] +
+        [Name(station, 'raw', f'ZFACTOR{i + 1}_A42') for i in range(7)] +
+        [Name(station, 'raw', f'Ir{i + 1}_A42') for i in range(7)]
+    )
+)
+station_profile_export['aerosol']['clean']['aethalometer'].data = lambda station, start_epoch_ms, end_epoch_ms, directory: DataExport(
+    start_epoch_ms, end_epoch_ms, directory, 'unsplit', set(
+        [Name(station, 'clean', f'Ba{i + 1}_A42') for i in range(7)] +
+        [Name(station, 'clean', f'X{i + 1}_A42') for i in range(7)] +
+        [Name(station, 'clean', f'ZFACTOR{i + 1}_A42') for i in range(7)] +
+        [Name(station, 'clean', f'Ir{i + 1}_A42') for i in range(7)]
+    )
+)
+station_profile_export['aerosol']['avgh']['aethalometer'].data = lambda station, start_epoch_ms, end_epoch_ms, directory: DataExport(
+    start_epoch_ms, end_epoch_ms, directory, 'average', set(
+        [Name(station, 'avgh', f'Ba{i + 1}_A42') for i in range(7)] +
+        [Name(station, 'avgh', f'X{i + 1}_A42') for i in range(7)] +
+        [Name(station, 'avgh', f'ZFACTOR{i + 1}_A42') for i in range(7)] +
+        [Name(station, 'avgh', f'Ir{i + 1}_A42') for i in range(7)]
+    )
+)
+
+
+def get(station: str, mode_name: str, export_key: str,
+        start_epoch_ms: int, end_epoch_ms: int, directory: str) -> typing.Optional[Export]:
+    return export_profile_get(station, mode_name, export_key,
+                              start_epoch_ms, end_epoch_ms, directory, station_profile_export)
+
+
+async def visible(station: str, mode_name: str) -> typing.Optional[ExportList]:
+    return export_profile_lookup(station, mode_name, station_profile_export)
