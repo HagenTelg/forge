68943b8cf51529d2aa1f6a9f13248c019554a743
Derek Hageman <Derek.Hageman@noaa.gov>
2023-04-17 08:36:15 -0600

    Still apply radiation limit to diffuse/global
    Plots look unreasonable without it.

--- forge/vis/station/default/radiation/ratio.py
+++ forge/vis/station/default/radiation/ratio.py
@@ -179,6 +179,7 @@ function calc(pir, temperature) {
 
         ratio = SolarTimeSeries.Axis()
         ratio.format_code = '.3f'
+        ratio.range = [0.0, 5.0]
         diffuse_global.axes.append(ratio)
 
         trace = SolarTimeSeries.Trace(ratio)
