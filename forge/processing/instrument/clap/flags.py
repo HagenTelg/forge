1f409b0cc86dab0754b42f846f2879c46a333c18
Derek Hageman <Derek.Hageman@noaa.gov>
2023-05-09 09:38:08 -0600

    Change CLAP dashboard flag severity

--- forge/processing/instrument/clap/flags.py
+++ forge/processing/instrument/clap/flags.py
@@ -5,7 +5,7 @@ from ..default.flags import DashboardFlag, Severity
 dashboard_flags: typing.Dict[str, DashboardFlag] = {
     'flow_error': DashboardFlag(Severity.ERROR, "Flow error", "Check valve and pump status"),
     'led_error': DashboardFlag(Severity.ERROR, "LED error"),
-    'filter_was_not_white': DashboardFlag(Severity.ERROR, "Initial filter parameters where not close to white",
+    'filter_was_not_white': DashboardFlag(Severity.WARNING, "Initial filter parameters where not close to white",
                                           "Change the filter and verify normal sampling if ongoing"),
-    'temperature_out_of_range': DashboardFlag(Severity.ERROR, "Temperature out of range"),
+    'temperature_out_of_range': DashboardFlag(Severity.WARNING, "Temperature out of range"),
 }
