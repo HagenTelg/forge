3cc2dc6cc7dfc72f709e490fa88fe5bff87b51aa
Derek Hageman <Derek.Hageman@noaa.gov>
2023-07-05 07:40:17 -0600

    Also update the description for the pulse height
    Missed that the first time.

--- forge/processing/instrument/admagic250cpc/flags.py
+++ forge/processing/instrument/admagic250cpc/flags.py
@@ -3,6 +3,6 @@ from ..default.flags import DashboardFlag, Severity
 
 
 dashboard_flags: typing.Dict[str, DashboardFlag] = {
-    'pulse_height_low': DashboardFlag(Severity.WARNING, "Pulse height low (less than 800mV)",
+    'pulse_height_low': DashboardFlag(Severity.WARNING, "Pulse height low (less than 400mV)",
                                       "Wick wetting may be required", instrument_flag=False),
 }
