1d76cbe2cd298190bf88d094dc37ea59d058443c
Derek Hageman <Derek.Hageman@noaa.gov>
2023-07-31 10:46:47 -0600

    Add translation for filter contamination
    Add a CPD3 flag translations for filter contamination flags.

---
+++ forge/cpd3/convert/instrument/filtercarousel/flags.py
@@ -0,0 +1,10 @@
+import typing
+from ..default.flags import CPD3Flag
+
+
+lookup: typing.Dict[str, CPD3Flag] = {
+    'cnc_high': CPD3Flag("ContaminateCPCHigh", "Data contaminated due to high particle concentration", 0x1),
+    'cnc_spike': CPD3Flag("ContaminateCPCSpike", "Data contaminated due to a spike in particle concentration", 0x1),
+    'wind_out_of_sector': CPD3Flag("ContaminateRealtimeWindDirection", "Data contaminated due to winds out of sector", 0x5),
+    'wind_speed_low': CPD3Flag("ContaminateRealtimeWindSpeed", "Data contaminated due to low wind speed", 0x5),
+}
