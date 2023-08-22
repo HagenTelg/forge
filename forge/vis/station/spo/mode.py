b16cb1399b16e053e98e265d7ad0ab3e56d59503
Derek Hageman <Derek.Hageman@noaa.gov>
2023-08-22 10:37:49 -0600

    Enable SPO DMPS editing tab
    DMPS data is part of editing now for SPO.

--- forge/vis/station/spo/mode.py
+++ forge/vis/station/spo/mode.py
@@ -11,12 +11,12 @@ station_modes['aerosol-realtime'].insert(ViewList.Entry('aerosol-realtime-cpcsta
 
 station_modes['aerosol-raw'].insert(ViewList.Entry('aerosol-raw-dmps', "DMPS"),
                                     'aerosol-raw-aethalometer')
-# station_modes['aerosol-editing'].insert(ViewList.Entry('aerosol-editing-dmps', "DMPS"),
-#                                         'aerosol-editing-absorption')
-# station_modes['aerosol-clean'].insert(ViewList.Entry('aerosol-clean-dmps', "DMPS"),
-#                                       'aerosol-clean-aethalometer')
-# station_modes['aerosol-avgh'].insert(ViewList.Entry('aerosol-avgh-dmps', "DMPS"),
-#                                      'aerosol-avgh-aethalometer')
+station_modes['aerosol-editing'].insert(ViewList.Entry('aerosol-editing-dmps', "DMPS"),
+                                        'aerosol-editing-absorption')
+station_modes['aerosol-clean'].insert(ViewList.Entry('aerosol-clean-dmps', "DMPS"),
+                                      'aerosol-clean-aethalometer')
+station_modes['aerosol-avgh'].insert(ViewList.Entry('aerosol-avgh-dmps', "DMPS"),
+                                     'aerosol-avgh-aethalometer')
 station_modes['aerosol-raw'].insert(ViewList.Entry('aerosol-raw-dmpsstatus', "DMPS Status"),
                                     'aerosol-raw-cpcstatus2')
 
