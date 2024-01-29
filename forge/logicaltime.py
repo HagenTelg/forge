768aeb4603e446dd83c201ff5a06227496e6ed9d
Derek Hageman <Derek.Hageman@noaa.gov>
2024-01-29 10:47:09 -0700

    fixup! Add averaging support

--- forge/logicaltime.py
+++ forge/logicaltime.py
@@ -1,7 +1,7 @@
 import typing
 import datetime
 import time
-from math import floor, ceil
+from math import floor
 
 
 def start_of_year(year: int) -> int:
@@ -47,3 +47,69 @@ def containing_year_range(start: float, end: float) -> typing.Tuple[int, int]:
 def round_to_year(start: float, end: float) -> typing.Tuple[int, int]:
     start_year_number, end_year_number = containing_year_range(start, end)
     return start_of_year(start_year_number), start_of_year(end_year_number)
+
+
+def start_of_month(year: int, month: int) -> int:
+    return int(floor(datetime.datetime(
+        year, month, 1,
+        tzinfo=datetime.timezone.utc
+    ).timestamp()))
+
+
+def start_of_month_ms(year: int, month: int) -> int:
+    return int(floor(start_of_month(year, month) * 1000.0))
+
+
+def end_of_month(year: int, month: int) -> int:
+    month += 1
+    if month > 12:
+        month = 1
+        year += 1
+    return start_of_month(year, month)
+
+
+def end_of_month_ms(year: int, month: int) -> int:
+    return int(floor(end_of_month(year, month) * 1000.0))
+
+
+def month_bounds(year: int, month: int) -> typing.Tuple[int, int]:
+    return start_of_month(year, month), end_of_month(year, month)
+
+
+def month_bounds_ms(year: int, month: int) -> typing.Tuple[int, int]:
+    start, end = month_bounds(year, month)
+    return start * 1000, end * 1000
+
+
+def months_since_epoch(epoch: float) -> int:
+    ts = time.gmtime(epoch)
+    return (ts.tm_year - 1970) * 12 + (ts.tm_mon - 1)
+
+
+def year_month_from_epoch_month(epoch_months: int) -> typing.Tuple[int, int]:
+    year = int(floor(epoch_months / 12)) + 1970
+    month = int(epoch_months % 12) + 1
+    return year, month
+
+
+def start_of_epoch_month(epoch_months: int) -> int:
+    return start_of_month(*year_month_from_epoch_month(epoch_months))
+
+
+def start_of_epoch_month_ms(epoch_months: int) -> int:
+    return start_of_epoch_month(epoch_months) * 1000
+
+
+def containing_epoch_month_range(start: float, end: float) -> typing.Tuple[int, int]:
+    start_month_number = months_since_epoch(start)
+    end_month_number = months_since_epoch(end)
+    month_end = start_of_epoch_month(end_month_number)
+    if month_end < end:
+        end_month_number += 1
+    return start_month_number, end_month_number
+
+
+def round_to_month(start: float, end: float) -> typing.Tuple[int, int]:
+    start_month_number, end_month_number = containing_epoch_month_range(start, end)
+    return start_of_epoch_month(start_month_number), start_of_epoch_month(end_month_number)
+
--- forge/processing/average/calculate.py
+++ forge/processing/average/calculate.py
@@ -1,6 +1,8 @@
 import typing
+from abc import ABC, abstractmethod
 from math import nan, inf
 import numpy as np
+from forge.logicaltime import containing_epoch_month_range, start_of_epoch_month_ms
 
 
 def _bin_weighted_average(bin_start: np.ndarray, values: np.ndarray, weights: np.ndarray) -> np.ndarray:
@@ -25,6 +27,17 @@ def _bin_weighted_average(bin_start: np.ndarray, values: np.ndarray, weights: np
     return sum_values
 
 
+def _fixed_interval_bins(times: np.ndarray, interval: typing.Union[int, float]) -> typing.Tuple[np.ndarray, np.ndarray]:
+    bin_numbers = np.empty_like(times, dtype=np.int64)
+    np.floor(times / interval, out=bin_numbers, casting='unsafe')
+    return np.unique(bin_numbers, return_index=True)
+
+
+def _fixed_interval_times(bin_numbers: np.ndarray, dt, interval: typing.Union[int, float]) -> np.ndarray:
+    bin_times = np.empty_like(bin_numbers, dtype=dt)
+    return np.multiply(bin_numbers, interval, out=bin_times, casting='unsafe')
+
+
 def fixed_interval_weighted_average(
         times: np.ndarray,
         values: np.ndarray,
@@ -41,12 +54,9 @@ def fixed_interval_weighted_average(
     :returns: a tuple of the averaged values, and the average start times
     """
 
-    bin_numbers = np.empty_like(times, dtype=np.int64)
-    np.floor(times / interval, out=bin_numbers, casting='unsafe')
-    bin_numbers, bin_start = np.unique(bin_numbers, return_index=True)
+    bin_numbers, bin_start = _fixed_interval_bins(times, interval)
     average = _bin_weighted_average(bin_start, values, weights)
-    bin_times = np.empty_like(bin_numbers, dtype=times.dtype)
-    np.multiply(bin_numbers, interval, out=bin_times, casting='unsafe')
+    bin_times = _fixed_interval_times(bin_numbers, times.dtype, interval)
     return average, bin_times
 
 
@@ -108,3 +118,106 @@ def fixed_interval_cover_average(
         fixed_interval_coverage_weight(times, averaged_time, nominal_spacing),
         interval
     )
+
+
+def _month_bins(times_epoch_ms: np.ndarray):
+    if times_epoch_ms.shape[0] < 1:
+        return np.empty_like(times_epoch_ms, dtype=np.int64), np.empty_like(times_epoch_ms, dtype=np.int64)
+
+    begin_index = 0
+    bin_numbers: typing.List[int] = list()
+    bin_start: typing.List[int] = list()
+    for month_number in range(
+            *containing_epoch_month_range(
+                float(times_epoch_ms[0] / 1000.0),
+                float(times_epoch_ms[-1] / 1000.0))
+    ):
+        end_time_ms = start_of_epoch_month_ms(month_number + 1)
+        end_index = np.searchsorted(times_epoch_ms[begin_index:], end_time_ms, side='right') + begin_index
+        if end_index == begin_index:
+            continue
+        bin_numbers.append(month_number)
+        bin_start.append(begin_index)
+
+    return np.array(bin_numbers), np.array(bin_start)
+
+
+_month_times = np.vectorize(start_of_epoch_month_ms)
+
+
+def month_weighted_average(
+        times_epoch_ms: np.ndarray,
+        values: np.ndarray,
+        weights: np.ndarray,
+) -> typing.Tuple[np.ndarray, np.ndarray]:
+    """
+    Calculate one month weighted averages.
+
+    :param times_epoch_ms: the times in ms since the epoch that the values and weights start at
+    :param values: the values to average
+    :param weights: the weights to apply
+    :returns: a tuple of the averaged values, and the average start times
+    """
+
+    bin_numbers, bin_start = _month_bins(times_epoch_ms)
+    average = _bin_weighted_average(bin_start, values, weights)
+    bin_times = _month_times(bin_numbers)
+    return average, bin_times
+
+
+def month_cover_average(
+        times_epoch_ms: np.ndarray,
+        values: np.ndarray,
+        averaged_time_ms: np.ndarray,
+        nominal_spacing_ms: typing.Optional[typing.Union[int, float]] = None,
+) -> typing.Tuple[np.ndarray, np.ndarray]:
+    """
+    Calculate a monthly average with weighting for values and the amount of time they represent.
+
+    :param times_epoch_ms: the times in ms since the epoch that the values and averaged times start at
+    :param values: the values to average
+    :param averaged_time_ms: the amount of time each value represents in ms
+    :param nominal_spacing_ms: the nominal time between each value in ms
+    :returns: a tuple of the averaged values, and the average start times
+    """
+
+    return month_weighted_average(
+        times_epoch_ms,
+        values,
+        fixed_interval_coverage_weight(times_epoch_ms, averaged_time_ms, nominal_spacing_ms),
+    )
+
+
+class FileAverager(ABC):
+    """
+    A class that handles averaging for the general file format.
+    """
+    def __init__(
+            self,
+            times_epoch_ms: np.ndarray,
+            averaged_time_ms: typing.Optional[np.ndarray] = None,
+            nominal_spacing_ms: typing.Optional[typing.Union[int, float]] = None,
+    ):
+        """
+        Construct the averager.
+
+        :param times_epoch_ms: the times in ms since the epoch that the values and averaged times start at
+        :param averaged_time: the amount of time each value represents
+        :param nominal_spacing_ms: the nominal time between each value in ms
+        """
+        self._original_times = times_epoch_ms
+        if averaged_time_ms:
+            self._weights = fixed_interval_coverage_weight(times_epoch_ms, averaged_time_ms, nominal_spacing_ms)
+        else:
+            self._weights = np.full(times_epoch_ms.shape, 1.0, dtype=np.float64)
+
+        bin_numbers, bin_start = self.calculate_bins()
+        self._bin_numbers = bin_numbers
+        self._bin_start = bin_start
+
+    @abstractmethod
+    def calculate_bins(self) -> typing.Tuple[np.ndarray, np.ndarray]:
+        pass
+
+    def __call__(self, values: np.ndarray) -> np.ndarray:
+        return _bin_weighted_average(self._bin_start, values, self._weights)
