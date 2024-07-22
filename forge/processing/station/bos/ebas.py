e3a484bb5132814223724b3e13dfe9a957917f86
Derek Hageman <Derek.Hageman@noaa.gov>
2024-07-22 12:05:27 -0600

    Add more EBAS station fields
    Additional fields for file generation.

--- forge/processing/station/bos/ebas.py
+++ forge/processing/station/bos/ebas.py
@@ -7,3 +7,23 @@ def station(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> 
 
 def platform(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
     return "US0901S"
+
+
+def land_use(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
+    return "Desert"
+
+
+def setting(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
+    return "Mountain"
+
+
+def other_identifiers(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
+    return "34 (BSRN), Boulder (AERONET)"
+
+
+def wmo_region(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[int]:
+    return 4
+
+
+def gaw_type(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
+    return "C"
--- forge/processing/station/default/ebas.py
+++ forge/processing/station/default/ebas.py
@@ -9,6 +9,10 @@ def platform(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) ->
     return None
 
 
+def lab_code(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
+    return "US06L"
+
+
 def land_use(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
     return None
 
@@ -19,3 +23,22 @@ def setting(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> 
 
 def other_identifiers(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
     return None
+
+
+def wmo_region(station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[int]:
+    return None
+
+
+def gaw_type(station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
+    return None
+
+
+def wdca_id(station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
+    from forge.processing.station.lookup import station_data
+    cc = station_data(station, 'site', 'country_code')(station, tags)
+    if not cc:
+        cc = "__"
+    sd = station_data(station, 'site', 'subdivision')(station, tags)
+    if not sd:
+        sd = "__"
+    return f"GAWA{cc}{sd}{station.upper()}"
