6a06b72953eba41bfaddb4fd5d7d6f731902c0a5
Derek Hageman <Derek.Hageman@noaa.gov>
2024-07-30 10:13:54 -0600

    Add SUM Ecotech neph plots
    Add plots for the Ecotech neph.

--- forge/vis/station/sum/mode.py
+++ forge/vis/station/sum/mode.py
@@ -12,5 +12,28 @@ station_modes['aerosol-realtime'].remove('aerosol-realtime-wind')
 station_modes['ozone-realtime'].remove('ozone-realtime-wind')
 
 
+station_modes['aerosol-raw'].insert(ViewList.Entry('aerosol-raw-opticalscattering2', "Ecotech Optical"),
+                                    'aerosol-raw-optical')
+station_modes['aerosol-realtime'].insert(ViewList.Entry('aerosol-realtime-opticalscattering2', "Ecotech Optical"),
+                                         'aerosol-realtime-optical')
+station_modes['aerosol-clean'].insert(ViewList.Entry('aerosol-clean-opticalscattering2', "Ecotech Optical"),
+                                      'aerosol-clean-optical')
+station_modes['aerosol-avgh'].insert(ViewList.Entry('aerosol-avgh-opticalscattering2', "Ecotech Optical"),
+                                     'aerosol-avgh-optical')
+
+station_modes['aerosol-raw'].insert(ViewList.Entry('aerosol-raw-nephelometerzero2', "Ecotech Nephelometer Zero"),
+                                    'aerosol-raw-nephelometerstatus')
+station_modes['aerosol-raw'].insert(ViewList.Entry('aerosol-raw-nephelometerstatus2', "Ecotech Nephelometer Status"),
+                                    'aerosol-raw-nephelometerzero2')
+station_modes['aerosol-realtime'].insert(ViewList.Entry('aerosol-realtime-nephelometerzero2', "Ecotech Nephelometer Zero"),
+                                    'aerosol-realtime-nephelometerstatus')
+station_modes['aerosol-realtime'].insert(ViewList.Entry('aerosol-realtime-nephelometerstatus2', "Ecotech Nephelometer Status"),
+                                    'aerosol-realtime-nephelometerzero2')
+station_modes['aerosol-editing'].insert(ViewList.Entry('aerosol-editing-scattering2', "Ecotech Scattering"),
+                                        'aerosol-editing-backscattering')
+station_modes['aerosol-editing'].insert(ViewList.Entry('aerosol-editing-backscattering2', "Ecotech Back Scattering"),
+                                        'aerosol-editing-scattering2')
+
+
 def get(station: str, mode_name: str) -> typing.Optional[Mode]:
     return station_modes.get(mode_name)
--- forge/vis/station/sum/view.py
+++ forge/vis/station/sum/view.py
@@ -7,6 +7,8 @@ from ..default.met.editing.wind import EditingWindSpeed as MetEditingWindSpeed
 from ..default.met.editing.wind import EditingWindDirection as MetEditingWindDirection
 from ..default.radiation.ambient import Ambient as RadiationAmbient
 from .pressure import Pressure
+from .ecotechnephelometer import NephelometerStatusSecondary, NephelometerZeroSecondary
+from .optical import OpticalScatteringSecondary, EditingScatteringSecondary, EditingBackScatteringSecondary
 
 
 station_views = detach(aerosol_views, ozone_views, met_views, radiation_views)
@@ -16,6 +18,20 @@ station_views['aerosol-raw-pressure'] = Pressure('aerosol-raw')
 station_views['aerosol-raw-cpcstatus'] = ADMagicCPC200Status('aerosol-raw')
 station_views['aerosol-realtime-pressure'] = Pressure('aerosol-realtime', realtime=True)
 station_views['aerosol-realtime-cpcstatus'] = ADMagicCPC200Status('aerosol-realtime', realtime=True)
+
+
+station_views['aerosol-raw-opticalscattering2'] = OpticalScatteringSecondary('aerosol-raw')
+station_views['aerosol-realtime-opticalscattering2'] = OpticalScatteringSecondary('aerosol-realtime', realtime=True)
+station_views['aerosol-editing-scattering2'] = EditingScatteringSecondary()
+station_views['aerosol-editing-backscattering2'] = EditingBackScatteringSecondary()
+station_views['aerosol-clean-opticalscattering2'] = OpticalScatteringSecondary('aerosol-clean')
+station_views['aerosol-avgh-opticalscattering2'] = OpticalScatteringSecondary('aerosol-avgh')
+station_views['aerosol-raw-nephelometerzero2'] = NephelometerZeroSecondary('aerosol-raw')
+station_views['aerosol-raw-nephelometerstatus2'] = NephelometerStatusSecondary('aerosol-raw')
+station_views['aerosol-raw-nephelometerzero2'] = NephelometerZeroSecondary('aerosol-raw')
+station_views['aerosol-raw-nephelometerstatus2'] = NephelometerStatusSecondary('aerosol-raw')
+station_views['aerosol-realtime-nephelometerzero2'] = NephelometerZeroSecondary('aerosol-realtime', realtime=True)
+station_views['aerosol-realtime-nephelometerstatus2'] = NephelometerStatusSecondary('aerosol-realtime', realtime=True)
 
 
 wind_measurements = OrderedDict([
