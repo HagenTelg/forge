0b77efaa4818f48daaba5ef67ee4f50260230d80
Derek Hageman <Derek.Hageman@noaa.gov>
2024-08-20 07:18:33 -0600

    Add MBO S12 export
    Add Ecotech neph to scattering exports.

--- forge/vis/station/mbo/export.py
+++ forge/vis/station/mbo/export.py
@@ -5,6 +5,73 @@ from forge.vis.station.cpd3 import Name, DataExport, DataExportList, detach, pro
 
 station_profile_export = detach(profile_export)
 
+station_profile_export['aerosol']['raw']['scattering'].data = lambda station, start_epoch_ms, end_epoch_ms, directory: DataExport(
+    start_epoch_ms, end_epoch_ms, directory, 'basic', {
+        Name(station, 'raw', 'T_S11'),
+        Name(station, 'raw', 'P_S11'),
+        Name(station, 'raw', 'U_S11'),
+        Name(station, 'raw', 'T_S12'),
+        Name(station, 'raw', 'P_S12'),
+        Name(station, 'raw', 'U_S12'),
+        Name(station, 'raw', 'BsB_S11'),
+        Name(station, 'raw', 'BsG_S11'),
+        Name(station, 'raw', 'BsR_S11'),
+        Name(station, 'raw', 'BbsB_S11'),
+        Name(station, 'raw', 'BbsG_S11'),
+        Name(station, 'raw', 'BbsR_S11'),
+        Name(station, 'raw', 'BsB_S12'),
+        Name(station, 'raw', 'BsG_S12'),
+        Name(station, 'raw', 'BsR_S12'),
+        Name(station, 'raw', 'BbsB_S12'),
+        Name(station, 'raw', 'BbsG_S12'),
+        Name(station, 'raw', 'BbsR_S12'),
+    },
+)
+station_profile_export['aerosol']['clean']['scattering'].data = lambda station, start_epoch_ms, end_epoch_ms, directory: DataExport(
+    start_epoch_ms, end_epoch_ms, directory, 'basic', {
+        Name(station, 'clean', 'T_S11'),
+        Name(station, 'clean', 'P_S11'),
+        Name(station, 'clean', 'U_S11'),
+        Name(station, 'clean', 'T_S12'),
+        Name(station, 'clean', 'P_S12'),
+        Name(station, 'clean', 'U_S12'),
+        Name(station, 'clean', 'BsB_S11'),
+        Name(station, 'clean', 'BsG_S11'),
+        Name(station, 'clean', 'BsR_S11'),
+        Name(station, 'clean', 'BbsB_S11'),
+        Name(station, 'clean', 'BbsG_S11'),
+        Name(station, 'clean', 'BbsR_S11'),
+        Name(station, 'clean', 'BsB_S12'),
+        Name(station, 'clean', 'BsG_S12'),
+        Name(station, 'clean', 'BsR_S12'),
+        Name(station, 'clean', 'BbsB_S12'),
+        Name(station, 'clean', 'BbsG_S12'),
+        Name(station, 'clean', 'BbsR_S12'),
+    },
+)
+station_profile_export['aerosol']['avgh']['scattering'].data = lambda station, start_epoch_ms, end_epoch_ms, directory: DataExport(
+    start_epoch_ms, end_epoch_ms, directory, 'basic', {
+        Name(station, 'avgh', 'T_S11'),
+        Name(station, 'avgh', 'P_S11'),
+        Name(station, 'avgh', 'U_S11'),
+        Name(station, 'avgh', 'T_S12'),
+        Name(station, 'avgh', 'P_S12'),
+        Name(station, 'avgh', 'U_S12'),
+        Name(station, 'avgh', 'BsB_S11'),
+        Name(station, 'avgh', 'BsG_S11'),
+        Name(station, 'avgh', 'BsR_S11'),
+        Name(station, 'avgh', 'BbsB_S11'),
+        Name(station, 'avgh', 'BbsG_S11'),
+        Name(station, 'avgh', 'BbsR_S11'),
+        Name(station, 'avgh', 'BsB_S12'),
+        Name(station, 'avgh', 'BsG_S12'),
+        Name(station, 'avgh', 'BsR_S12'),
+        Name(station, 'avgh', 'BbsB_S12'),
+        Name(station, 'avgh', 'BbsG_S12'),
+        Name(station, 'avgh', 'BbsR_S12'),
+    },
+)
+
 station_profile_export['aerosol']['raw'].insert(
     DataExportList.Entry('met', "Meteorological", lambda station, start_epoch_ms, end_epoch_ms, directory: DataExport(
         start_epoch_ms, end_epoch_ms, directory, 'unsplit', {
