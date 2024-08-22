11133b6cd26632f7df36b66d8354b3d5d89fbea6
Derek Hageman <Derek.Hageman@noaa.gov>
2024-08-22 11:07:18 -0600

    Add dilution flow to ARN exports
    Add flows for dilution to ARN scattering exports.

--- forge/vis/station/arn/export.py
+++ forge/vis/station/arn/export.py
@@ -20,6 +20,52 @@ station_profile_export['aerosol']['avgh']['counts'].data = lambda station, start
     },
 )
 
+station_profile_export['aerosol']['raw']['scattering'].data = lambda station, start_epoch_ms, end_epoch_ms, directory: DataExport(
+    start_epoch_ms, end_epoch_ms, directory, 'basic', {
+        Name(station, 'raw', 'T_S11'),
+        Name(station, 'raw', 'P_S11'),
+        Name(station, 'raw', 'U_S11'),
+        Name(station, 'raw', 'BsB_S11'),
+        Name(station, 'raw', 'BsG_S11'),
+        Name(station, 'raw', 'BsR_S11'),
+        Name(station, 'raw', 'BbsB_S11'),
+        Name(station, 'raw', 'BbsG_S11'),
+        Name(station, 'raw', 'BbsR_S11'),
+        Name(station, 'raw', 'Q_Q11'),
+        Name(station, 'raw', 'Q_Q12'),
+    },
+)
+station_profile_export['aerosol']['clean']['scattering'].data = lambda station, start_epoch_ms, end_epoch_ms, directory: DataExport(
+    start_epoch_ms, end_epoch_ms, directory, 'basic', {
+        Name(station, 'clean', 'T_S11'),
+        Name(station, 'clean', 'P_S11'),
+        Name(station, 'clean', 'U_S11'),
+        Name(station, 'clean', 'BsB_S11'),
+        Name(station, 'clean', 'BsG_S11'),
+        Name(station, 'clean', 'BsR_S11'),
+        Name(station, 'clean', 'BbsB_S11'),
+        Name(station, 'clean', 'BbsG_S11'),
+        Name(station, 'clean', 'BbsR_S11'),
+        Name(station, 'clean', 'Q_Q11'),
+        Name(station, 'clean', 'Q_Q12'),
+    },
+)
+station_profile_export['aerosol']['avgh']['scattering'].data = lambda station, start_epoch_ms, end_epoch_ms, directory: DataExport(
+    start_epoch_ms, end_epoch_ms, directory, 'basic', {
+        Name(station, 'avgh', 'T_S11'),
+        Name(station, 'avgh', 'P_S11'),
+        Name(station, 'avgh', 'U_S11'),
+        Name(station, 'avgh', 'BsB_S11'),
+        Name(station, 'avgh', 'BsG_S11'),
+        Name(station, 'avgh', 'BsR_S11'),
+        Name(station, 'avgh', 'BbsB_S11'),
+        Name(station, 'avgh', 'BbsG_S11'),
+        Name(station, 'avgh', 'BbsR_S11'),
+        Name(station, 'avgh', 'Q_Q11'),
+        Name(station, 'avgh', 'Q_Q12'),
+    },
+)
+
 
 def get(station: str, mode_name: str, export_key: str,
         start_epoch_ms: int, end_epoch_ms: int, directory: str) -> typing.Optional[Export]:
