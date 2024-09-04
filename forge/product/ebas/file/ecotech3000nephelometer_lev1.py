b787136d56506e1a9fd26fad436343b52eee3f29
Derek Hageman <Derek.Hageman@noaa.gov>
2024-09-04 12:22:27 -0600

    Add Ecotech neph EBAS definitions
    Add file definitions for the Ecotech 3000 neph.

---
+++ forge/product/ebas/file/ecotech3000nephelometer_lev1.py
@@ -0,0 +1,29 @@
+import typing
+from .scattering import Level1File
+
+
+class File(Level1File):
+    @property
+    def tags(self) -> typing.Optional[typing.Set[str]]:
+        return {"aerosol", "scattering", "ecotechnephelometer"}
+
+    @property
+    def instrument_manufacturer(self) -> str:
+        return "Ecotech"
+
+    @property
+    def instrument_model(self) -> str:
+        return "3000"
+
+    @property
+    def instrument_name(self) -> str:
+        return f'Ecotech_3000_{self.station.upper()}'
+
+    @property
+    def file_metadata(self) -> typing.Dict[str, str]:
+        r = super().file_metadata
+        r.update({
+            'std_method': 'cal-gas=CO2+AIR_truncation-correction=Mueller2011',
+            'comment': 'Standard Mueller 2011 values used for truncation correction',
+        })
+        return r
