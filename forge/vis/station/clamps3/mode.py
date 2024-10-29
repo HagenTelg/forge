2a5ae11d63a6f9f8603377d62eb25f102f502225
Derek Hageman <Derek.Hageman@noaa.gov>
2024-10-29 16:35:57 -0600

    Fix CLAMPS trailer realtime display
    Missing realtime mode.

--- forge/vis/station/clamps3/mode.py
+++ forge/vis/station/clamps3/mode.py
@@ -1,10 +1,14 @@
 import typing
-from ..default.mode import Mode, ViewList, DefaultAcquisition
+from ..default.mode import Mode, ViewList, Realtime, DefaultAcquisition
 
 
 station_modes = {
     'aerosol-raw': ViewList('aerosol-raw', "Raw", [
         ViewList.Entry('aerosol-raw-mass', "Mass Concentration"),
+        ViewList.Entry('aerosol-raw-t640status', "T640 Status"),
+    ]),
+    'aerosol-realtime': Realtime('aerosol-realtime', "Realtime", [
+        ViewList.Entry('aerosol-realtime-mass', "Mass Concentration"),
         ViewList.Entry('aerosol-realtime-t640status', "T640 Status"),
     ]),
     'acquisition': DefaultAcquisition(),
