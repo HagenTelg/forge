1d3f1ab47e89cbbf72f7fb7cfb84f62fb41bc80e
Derek Hageman <Derek.Hageman@noaa.gov>
2024-12-17 10:29:27 -0700

    Add chunking for CPD3 analyzer
    Add a chunking option to deal with cases where there's a lot of
    metadata spam (communications failures).

--- forge/cpd3/legacy/raw/analyze.py
+++ forge/cpd3/legacy/raw/analyze.py
@@ -9,7 +9,7 @@ import forge.cpd3.variant as variant
 from math import floor, ceil, nan
 from forge.const import STATIONS as VALID_STATIONS
 from forge.formattime import format_iso8601_time
-from forge.timeparse import parse_time_argument
+from forge.timeparse import parse_time_argument, parse_interval_argument
 from forge.cpd3.convert.station.lookup import station_data
 from forge.cpd3.legacy.readarchive import read_archive, Selection
 
@@ -56,7 +56,8 @@ class Instrument:
 
     @classmethod
     def scan_station(cls, station: str, start: float, end: float,
-                     instrument_limit: typing.Optional[str] = None) -> typing.Dict[str, typing.List["Instrument"]]:
+                     instrument_limit: typing.Optional[str] = None,
+                     chunk_size: typing.Optional[float] = None) -> typing.Dict[str, typing.List["Instrument"]]:
         def get_path(root: variant.Metadata, *path) -> typing.Optional[typing.Any]:
             for p in path:
                 root = root.get(p)
@@ -64,80 +65,95 @@ class Instrument:
                     return None
             return root
 
+        def scan_chunks():
+            next_start = start
+            if not chunk_size:
+                yield start, end
+                return
+            while True:
+                next_end = next_start + chunk_size
+                if next_end >= end:
+                    yield next_start, end
+                    return
+                yield next_start, next_end
+                next_start = next_end
+
         result: typing.Dict[str, typing.List["Instrument"]] = dict()
-        for identity, value, _ in read_archive([Selection(
-                start=start,
-                end=end,
-                stations=[station],
-                archives=["raw_meta"],
-                include_meta_archive=False,
-                include_default_station=False,
-                **({'variables': [".*_" + instrument_limit]} if instrument_limit else {})
-        )]):
-            if identity.variable == "alias":
-                continue
-            if not isinstance(value, variant.Metadata):
-                continue
-            parts = identity.variable.split('_', 1)
-            if len(parts) != 2:
-                continue
+        for scan_start, scan_end in scan_chunks():
+            _LOGGER.debug(f"Scanning {scan_start} to {scan_end}")
+            for identity, value, _ in read_archive([Selection(
+                    start=scan_start,
+                    end=scan_end,
+                    stations=[station],
+                    archives=["raw_meta"],
+                    include_meta_archive=False,
+                    include_default_station=False,
+                    **({'variables': [".*_" + instrument_limit]} if instrument_limit else {})
+            )]):
+                if identity.variable == "alias":
+                    continue
+                if not isinstance(value, variant.Metadata):
+                    continue
+                parts = identity.variable.split('_', 1)
+                if len(parts) != 2:
+                    continue
 
-            instrument_id = get_path(value, "Source", "Name")
-            if not instrument_id:
-                instrument_id = parts[1]
-            instrument_id = str(instrument_id)
+                instrument_id = get_path(value, "Source", "Name")
+                if not instrument_id:
+                    instrument_id = parts[1]
+                instrument_id = str(instrument_id)
 
-            source_identifier = cls.Identifier()
-            source_identifier.cpd3_component = get_path(value, "Source", "Component")
-            source_identifier.forge_instrument = get_path(value, "Source", "ForgeInstrument")
+                source_identifier = cls.Identifier()
+                source_identifier.cpd3_component = get_path(value, "Source", "Component")
+                source_identifier.forge_instrument = get_path(value, "Source", "ForgeInstrument")
 
-            identifier: typing.Dict[str, typing.Any] = {
-                key: get_path(value, "Source", key) for key in ("Manufacturer", "Model", "SerialNumber")
-            }
+                identifier: typing.Dict[str, typing.Any] = {
+                    key: get_path(value, "Source", key) for key in ("Manufacturer", "Model", "SerialNumber")
+                }
 
-            instrument_segments = result.get(instrument_id)
-            if not instrument_segments:
-                instrument_segments = list()
-                result[instrument_id] = instrument_segments
-            else:
-                # Ignore state metadata, since it can be persisted longer than it should
-                smoothing_mode = get_path(value, "Smoothing", "Mode")
-                if smoothing_mode:
-                    smoothing_mode = str(smoothing_mode).lower()
-                    if smoothing_mode == "none" or smoothing_mode == "bypass":
-                        continue
+                instrument_segments = result.get(instrument_id)
+                if not instrument_segments:
+                    instrument_segments = list()
+                    result[instrument_id] = instrument_segments
+                else:
+                    # Ignore state metadata, since it can be persisted longer than it should
+                    smoothing_mode = get_path(value, "Smoothing", "Mode")
+                    if smoothing_mode:
+                        smoothing_mode = str(smoothing_mode).lower()
+                        if smoothing_mode == "none" or smoothing_mode == "bypass":
+                            continue
 
-            effective_start = max(start, identity.start) if identity.start else start
-            effective_end = min(end, identity.end) if identity.end else end
+                effective_start = max(scan_start, identity.start) if identity.start else scan_start
+                effective_end = min(scan_end, identity.end) if identity.end else scan_end
 
-            def can_merge(target: Instrument) -> bool:
-                if target.source != source_identifier:
-                    return False
-                if effective_end - effective_start < (2 * 60 * 60):
-                    return True
-                effective_start_day = int(floor(effective_start / (24 * 60 * 60)))
-                target_end_day = int(ceil(target.end / (24 * 60 * 60)))
-                if effective_start_day < target_end_day + 1:
-                    return True
-                # Since the analysis is about conversion type, ignore gaps as long as the identifier matches
-                return target._last_identifier == identifier
+                def can_merge(target: Instrument) -> bool:
+                    if target.source != source_identifier:
+                        return False
+                    if effective_end - effective_start < (2 * 60 * 60):
+                        return True
+                    effective_start_day = int(floor(effective_start / (24 * 60 * 60)))
+                    target_end_day = int(ceil(target.end / (24 * 60 * 60)))
+                    if effective_start_day < target_end_day + 1:
+                        return True
+                    # Since the analysis is about conversion type, ignore gaps as long as the identifier matches
+                    return target._last_identifier == identifier
 
-            if len(instrument_segments) == 0 or not can_merge(instrument_segments[-1]):
-                if len(instrument_segments) >= 1:
-                    instrument_segments[-1].end = min(instrument_segments[-1].end, effective_start)
+                if len(instrument_segments) == 0 or not can_merge(instrument_segments[-1]):
+                    if len(instrument_segments) >= 1:
+                        instrument_segments[-1].end = min(instrument_segments[-1].end, effective_start)
 
-                instrument_data = cls()
-                instrument_data.start = effective_start
-                instrument_data.end = effective_end
-                instrument_segments.append(instrument_data)
-            else:
-                instrument_data = instrument_segments[-1]
-                instrument_data.end = max(instrument_data.end, effective_end)
+                    instrument_data = cls()
+                    instrument_data.start = effective_start
+                    instrument_data.end = effective_end
+                    instrument_segments.append(instrument_data)
+                else:
+                    instrument_data = instrument_segments[-1]
+                    instrument_data.end = max(instrument_data.end, effective_end)
 
-            if source_identifier:
-                instrument_data.source = source_identifier
-            instrument_data.variables.add(identity.variable)
-            instrument_data._last_identifier = identifier
+                if source_identifier:
+                    instrument_data.source = source_identifier
+                instrument_data.variables.add(identity.variable)
+                instrument_data._last_identifier = identifier
 
         return result
 
@@ -153,6 +169,9 @@ def main():
     parser.add_argument('--end',
                         dest='end',
                         help="override end time")
+    parser.add_argument('--chunk',
+                        dest='chunk',
+                        help="scan chunk size")
     parser.add_argument('--variables',
                         dest='variables', action='store_true',
                         help="show constituent variables")
@@ -170,10 +189,11 @@ def main():
     assert station in VALID_STATIONS
     start_time = parse_time_argument(args.start).timestamp() if args.start else station_data(station, 'legacy', 'DATA_START_TIME')
     end_time = parse_time_argument(args.end).timestamp() if args.end else station_data(station, 'legacy', 'DATA_END_TIME')
+    chunk_size = parse_interval_argument(args.chunk) if args.chunk else None
 
     _LOGGER.debug(f"Starting raw data analysis for {station.upper()} in {format_iso8601_time(start_time)} to {format_iso8601_time(end_time)}")
     begin_time = time.monotonic()
-    available = Instrument.scan_station(station, start_time, end_time, args.instrument)
+    available = Instrument.scan_station(station, start_time, end_time, args.instrument, chunk_size)
     end_time = time.monotonic()
     _LOGGER.debug(f"Analysis of {len(available)} instruments completed in {(end_time - begin_time):.2f} seconds")
 
