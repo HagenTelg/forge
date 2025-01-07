e24fc57751debd0deb68cfcc248a0a33b8e1ef2d
Derek Hageman <Derek.Hageman@noaa.gov>
2025-01-07 14:40:53 -0700

    Set BRW/SMR CPD3 end time
    Set the end time for legacy data.

--- forge/cpd3/convert/station/brw/legacy.py
+++ forge/cpd3/convert/station/brw/legacy.py
@@ -4,4 +4,4 @@ from forge.timeparse import parse_iso8601_time
 
 
 DATA_START_TIME: float = parse_iso8601_time("1976-05-06").timestamp()
-DATA_END_TIME: float = parse_iso8601_time("2024-11-04T18:00:00Z").timestamp()
+DATA_END_TIME: float = parse_iso8601_time("2025-01-07T04:00:00.000Z").timestamp()
--- forge/cpd3/convert/station/smr/legacy.py
+++ forge/cpd3/convert/station/smr/legacy.py
@@ -4,4 +4,4 @@ from forge.timeparse import parse_iso8601_time
 
 
 DATA_START_TIME: float = parse_iso8601_time("2005-06-01").timestamp()
-DATA_END_TIME: float = time.time()
+DATA_END_TIME: float = parse_iso8601_time("2025-01-07T04:00:00.000Z").timestamp()
