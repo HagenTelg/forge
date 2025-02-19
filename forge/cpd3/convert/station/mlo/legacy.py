bde6b58cad837966b6d5ecd9d99cf8eedb67aca2
Derek Hageman <Derek.Hageman@noaa.gov>
2025-02-19 08:31:44 -0700

    Set LLN CPD3 end time

--- forge/cpd3/convert/station/mlo/legacy.py
+++ forge/cpd3/convert/station/mlo/legacy.py
@@ -4,4 +4,4 @@ from forge.timeparse import parse_iso8601_time
 
 
 DATA_START_TIME: float = parse_iso8601_time("1973-09-01").timestamp()
-#DATA_END_TIME: float = parse_iso8601_time("2024-11-04T18:00:00Z").timestamp()
+DATA_END_TIME: float = parse_iso8601_time("2025-02-19T15:00:00Z").timestamp()
