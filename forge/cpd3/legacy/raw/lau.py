a72a15adeec001f9fcd01cf4d7291a2d7b7b1531
Derek Hageman <Derek.Hageman@noaa.gov>
2025-03-03 11:54:25 -0700

    LAU CPD3 data conversion
    LAU Forge data ingest added.

---
+++ forge/cpd3/legacy/raw/lau.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import typing
+import os
+from forge.const import STATIONS as VALID_STATIONS
+from forge.cpd3.legacy.raw.write import InstrumentTimeConversion as C
+
+STATION = os.path.basename(__file__).split('.', 1)[0].lower()
+assert STATION in VALID_STATIONS
+
+
+C.run(STATION, {
+    "G81": [ C('thermo49'), ],
+    "G82": [ C('thermo49+secondary', start='2023-05-24', end='2023-08-31'), ],
+})
