85b413c604531f1f7a8cd61b627cadf6b2f90fe2
Derek Hageman <Derek.Hageman@noaa.gov>
2025-03-05 11:38:26 -0700

    NWR CPD3 data conversion
    NWR Forge data ingest added.

---
+++ forge/cpd3/legacy/raw/nwr.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import typing
+import os
+from forge.const import STATIONS as VALID_STATIONS
+from forge.cpd3.legacy.raw.write import InstrumentTimeConversion as C
+from forge.cpd3.legacy.instrument.thermo49 import Converter as Thermo49
+
+STATION = os.path.basename(__file__).split('.', 1)[0].lower()
+assert STATION in VALID_STATIONS
+
+
+C.run(STATION, {
+    "G81": [
+        C(Thermo49.with_instrument_override(serial_number="75578-380"), end='2018-01-01'),
+        C('thermo49', start='2018-01-01'),
+    ],
+})
