c92aac03038a066fbc4337820bb80d0c9ae74bb8
Derek Hageman <Derek.Hageman@noaa.gov>
2025-03-10 13:40:22 -0600

    Add ARH serial number
    Serial number from edits.

--- forge/cpd3/legacy/raw/arh.py
+++ forge/cpd3/legacy/raw/arh.py
@@ -4,6 +4,7 @@ import typing
 import os
 from forge.const import STATIONS as VALID_STATIONS
 from forge.cpd3.legacy.raw.write import InstrumentTimeConversion as C
+from forge.cpd3.legacy.instrument.thermo49 import Converter as Thermo49
 
 STATION = os.path.basename(__file__).split('.', 1)[0].lower()
 assert STATION in VALID_STATIONS
@@ -11,7 +12,8 @@ assert STATION in VALID_STATIONS
 
 C.run(STATION, {
     "G81": [
-        C('thermo49', end='2023-11-08T01:02:00Z'),
+        C('thermo49', end='2015-12-20'),
+        C(Thermo49.with_instrument_override(serial_number="75576380"), start='2015-12-20', end='2023-11-08T01:02:00Z'),
         C('thermo49iq', start='2024-01-04'),
     ],
     "G82": [ C('thermo49+secondary', start='2023-11-08', end='2024-01-13'), ],
