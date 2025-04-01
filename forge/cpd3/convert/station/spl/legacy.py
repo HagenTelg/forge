eb97226aa83a8ad6566a4b4a51c7443c5d508937
Derek Hageman <Derek.Hageman@noaa.gov>
2025-04-01 08:29:59 -0600

    Set SPL CPD3 ingest end time

--- forge/cpd3/convert/station/spl/legacy.py
+++ forge/cpd3/convert/station/spl/legacy.py
@@ -4,4 +4,4 @@ from forge.timeparse import parse_iso8601_time
 
 
 DATA_START_TIME: float = parse_iso8601_time("1998-02-12").timestamp()
-#DATA_END_TIME: float = parse_iso8601_time("2024-07-30").timestamp()
+DATA_END_TIME: float = parse_iso8601_time("2025-04-01T14:00:00Z").timestamp()
