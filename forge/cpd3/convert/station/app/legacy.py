f73817d77465ecef8d470bbd517a0e18b1bf7425
Derek Hageman <Derek.Hageman@noaa.gov>
2025-04-04 08:50:58 -0600

    Add APP CPD3 data end time

--- forge/cpd3/convert/station/app/legacy.py
+++ forge/cpd3/convert/station/app/legacy.py
@@ -4,4 +4,4 @@ from forge.timeparse import parse_iso8601_time
 
 
 DATA_START_TIME: float = parse_iso8601_time("2009-06-01").timestamp()
-# DATA_END_TIME: float = parse_iso8601_time("2025-02-19T15:00:00Z").timestamp()
+DATA_END_TIME: float = parse_iso8601_time("2025-04-04T14:00:00Z").timestamp()
