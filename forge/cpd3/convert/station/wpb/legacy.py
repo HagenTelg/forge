2c8a0c25abe18a240962b79e1a1387d5b59786e7
Derek Hageman <Derek.Hageman@noaa.gov>
2025-04-15 08:28:48 -0600

    Fix WPB legacy ingest end time
    More data was ingested in CPD3 due to station duplication.

--- forge/cpd3/convert/station/wpb/legacy.py
+++ forge/cpd3/convert/station/wpb/legacy.py
@@ -4,4 +4,4 @@ from forge.timeparse import parse_iso8601_time
 
 
 DATA_START_TIME: float = parse_iso8601_time("2022-01-25").timestamp()
-DATA_END_TIME: float = parse_iso8601_time("2025-04-14T19:00:00Z").timestamp()
+DATA_END_TIME: float = parse_iso8601_time("2025-04-15T14:00:00Z").timestamp()
