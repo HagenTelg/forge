8661bbe38ca9bccc92df40f9b28ba06216b12344
Derek Hageman <Derek.Hageman@noaa.gov>
2025-04-29 11:07:02 -0600

    Add THD wind copy
    Copy aerosol winds to XM1 for old data editing.

--- forge/processing/station/thd/editing.py
+++ forge/processing/station/thd/editing.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import typing
 import numpy as np
+import forge.data.structure.variable as netcdf_var
 from forge.processing.context import AvailableData
 from forge.processing.corrections import *
 from forge.processing.station.default.editing import standard_absorption_corrections, standard_scattering_corrections, standard_intensives, standard_meteorological, standard_stp_corrections
-from forge.data.flags import parse_flags, declare_flag
+from forge.data.flags import parse_flags
 
 
 def aerosol_contamination(data: AvailableData) -> None:
@@ -33,6 +34,30 @@ def aerosol_contamination(data: AvailableData) -> None:
 
 
 def run(data: AvailableData) -> None:
+    # Copy aerosol winds before OBOP system was installed
+    for met, umac in data.derive_output(
+            "XM1",
+            {"instrument_id": "X1"},
+            tags=("met", ),
+            start="2002-04-10", end="2007-01-11",
+    ):
+        for WD, WS in umac.select_variable(
+                {"variable_name": "WD_X1"},
+                {"variable_name": "WS_X1"},
+                commit_variable=False, commit_auxiliary=False):
+            with met.get_output(WD, "wind_speed") as wind_speed:
+                netcdf_var.variable_wind_speed(wind_speed.variable)
+                wind_speed.variable.variable_id = "WS"
+                wind_speed.variable.coverage_content_type = "physicalMeasurement"
+                wind_speed.variable.cell_methods = f"time: mean wind_direction: vector_direction"
+                wind_speed[:] = WS[:]
+            with met.get_output(WD, "wind_direction") as wind_direction:
+                netcdf_var.variable_wind_speed(wind_direction.variable)
+                wind_direction.variable.variable_id = "WD"
+                wind_direction.variable.coverage_content_type = "physicalMeasurement"
+                wind_direction.variable.cell_methods = f"time: mean wind_speed: vector_magnitude"
+                wind_direction[:] = WD[:]
+
     for met in data.select_instrument({"instrument_id": "XM1"}, start="2007-01-11"):
         meteorological_climatology_limits(
             met,
