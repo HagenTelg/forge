308a4bfead51c34180ee8cca5ecedf4e1bc93c7d
Derek Hageman <Derek.Hageman@noaa.gov>
2025-05-03 08:20:02 -0600

    Enable EBAS submission for BEO, GSN and LLN

--- forge/processing/station/beo/ebas.py
+++ forge/processing/station/beo/ebas.py
@@ -64,3 +64,13 @@ def originator(gaw_station: str, tags: typing.Optional[typing.Set[str]] = None) 
         PS_ADDR_COUNTRY="Bulgaria",
         PS_ORCID=None,
     )]
+
+
+def submit(gaw_station: str) -> typing.Dict[str, typing.Tuple[str, typing.List["InstrumentSelection"]]]:
+    from ..default.ebas import standard_submit
+    return standard_submit(gaw_station)
+
+
+def nrt(gaw_station: str) -> typing.Dict[str, typing.Tuple[str, typing.List["InstrumentSelection"], str, str]]:
+    from ..default.ebas import standard_nrt
+    return standard_nrt(gaw_station)
