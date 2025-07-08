69f91cd31cf2acf4bf206a2f8adfad0efa0c029e
Derek Hageman <Derek.Hageman@noaa.gov>
2025-07-08 10:50:02 -0600

    Fix SGP site metadata
    Site location was copy-pasted wrong.

--- forge/processing/station/sgp/site.py
+++ forge/processing/station/sgp/site.py
@@ -2,21 +2,24 @@ import typing
 
 
 def latitude(station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[float]:
-    return 67.97361
+    return 36.5999984741
 
 
 def longitude(station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[float]:
-    return 24.11583
+    return -97.5000
 
 
 def altitude(station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[float]:
-    return 560.0
+    return 318.0
 
 
 def country_code(station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
-    return "FI"
+    return "US"
+
+
+def subdivision(station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
+    return "OK"
 
 
 def name(station: str, tags: typing.Optional[typing.Set[str]] = None) -> typing.Optional[str]:
-    return "Pallas"
-
+    return "Southern Great Plains E13"
