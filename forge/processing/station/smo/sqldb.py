cc728ced2cf233255a0e781a12f8254692aae438
Derek Hageman <Derek.Hageman@noaa.gov>
2025-09-30 09:33:49 -0600

    Enable SMO SQL database updates
    Enable SMO database updates.

---
+++ forge/processing/station/smo/sqldb.py
@@ -0,0 +1,9 @@
+import typing
+
+if typing.TYPE_CHECKING:
+    from forge.product.selection import InstrumentSelection
+
+
+def updates(station: str) -> typing.Dict[str, typing.Tuple[str, typing.List["InstrumentSelection"]]]:
+    from ..default.sqldb import standard_updates
+    return standard_updates(station)
