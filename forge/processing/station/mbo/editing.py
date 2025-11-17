e240093b41e067d46c98338046e6c0f98d357ff9
Derek Hageman <hageman@inthat.cloud>
2025-11-17 14:28:15 +0200

    Update MBO processing
    Noah requested S12 be used for absorption corrections from
    2025-01-20 to 2025-05-14.

--- forge/processing/station/mbo/editing.py
+++ forge/processing/station/mbo/editing.py
@@ -2,12 +2,35 @@
 import typing
 from forge.processing.context import AvailableData
 from forge.processing.corrections import *
-from forge.processing.station.default.editing import standard_corrections, standard_intensives, standard_meteorological
+from forge.processing.station.default.editing import standard_absorption_corrections, standard_scattering_corrections, standard_intensives, standard_meteorological, standard_stp_corrections
+
+
+def absorption_corrections(data: AvailableData) -> None:
+    standard_absorption_corrections(data, start="2018-01-01", end="2025-01-20T13:00:00Z")
+
+    # Noah email 2025-11-12, use S12 for absorption corrections
+    for absorption, scattering in data.select_instrument((
+            {"instrument": "psap1w"},
+            {"instrument": "psap3w"},
+    ), {"instrument_id": "S12"}, start="2025-01-20T13:00:00Z", end="2025-05-14T15:54:00Z"):
+        remove_low_transmittance(absorption)
+        bond_1999(absorption, scattering)
+    for absorption, scattering in data.select_instrument((
+            {"instrument": "bmitap"},
+            {"instrument": "clap"},
+    ), {"instrument_id": "S12"}, start="2025-01-20T13:00:00Z", end="2025-05-14T15:54:00Z"):
+        remove_low_transmittance(absorption)
+        weiss(absorption)
+        bond_1999(absorption, scattering)
+
+    standard_absorption_corrections(data, start="2025-05-14T15:54:00Z")
 
 
 def run(data: AvailableData) -> None:
     # Old data ingest is "already corrected"
-    standard_corrections(data, start="2018-01-01")
+    standard_stp_corrections(data, start="2018-01-01")
+    absorption_corrections(data)
+    standard_scattering_corrections(data, start="2018-01-01")
 
     for S11, A11, A12, N71, N11, pid, dilution_flow in data.select_multiple(
             {"instrument_id": "S11"},
