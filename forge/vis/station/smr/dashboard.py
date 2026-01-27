692c9af65582483996ef34e9789cf6eb781d35fd
Derek Hageman <derek.hageman@helsinki.fi>
2026-01-27 12:40:03 +0200

    Add SMR MAAP dashboard entry
    Add interpretation for the SMR MAAP ingest.

---
+++ forge/vis/station/smr/dashboard.py
@@ -0,0 +1,13 @@
+import typing
+from ..default.dashboard import code_records as base_code_records, record as base_record, Record, FileIngestRecord
+
+
+code_records = dict(base_code_records)
+
+code_records['ingest-maap'] = FileIngestRecord.simple_override(
+    name="Ingest MAAP data",
+)
+
+
+def record(station: typing.Optional[str], code: str) -> typing.Optional[Record]:
+    return code_records.get(code) or base_record(station, code)
