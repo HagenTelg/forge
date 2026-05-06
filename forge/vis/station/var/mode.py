73f9076089a8c75d555cc0c4c633be7468a6a7bb
Derek Hageman <derek.hageman@helsinki.fi>
2026-05-06 09:05:02 +0300

    Add VAR AE36
    Add plots for VAR AE36.

--- forge/vis/station/var/mode.py
+++ forge/vis/station/var/mode.py
@@ -39,14 +39,6 @@ station_modes['aerosol-raw'].remove('aerosol-raw-umacstatus')
 station_modes['aerosol-realtime'].remove('aerosol-realtime-flow')
 station_modes['aerosol-realtime'].remove('aerosol-realtime-umacstatus')
 
-station_modes['aerosol-raw'].remove('aerosol-raw-aethalometer')
-station_modes['aerosol-raw'].remove('aerosol-raw-aethalometerstatus')
-station_modes['aerosol-realtime'].remove('aerosol-realtime-aethalometer')
-station_modes['aerosol-realtime'].remove('aerosol-realtime-aethalometerstatus')
-station_modes['aerosol-editing'].remove('aerosol-editing-aethalometer')
-station_modes['aerosol-clean'].remove('aerosol-clean-aethalometer')
-station_modes['aerosol-avgh'].remove('aerosol-avgh-aethalometer')
-
 
 def get(station: str, mode_name: str) -> typing.Optional[Mode]:
     return station_modes.get(mode_name)
