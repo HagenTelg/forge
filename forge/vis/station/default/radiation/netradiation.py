231a90105a20736722e67af3c33d7ca192dfadf9
Logan Soldo <lsoldo@pm.me>
2026-07-22 16:28:49 -0600

    Refactor net radiation calculations

--- forge/vis/station/default/radiation/netradiation.py
+++ forge/vis/station/default/radiation/netradiation.py
@@ -1,5 +1,7 @@
 import typing
+
 from forge.vis.view.solar import SolarTimeSeries
+
 
 class NetRadiation(SolarTimeSeries):
     class CalculateNetRadiation(SolarTimeSeries.Processing):
@@ -12,38 +14,49 @@ function net(direct, diffuse, up_sw, down_lw, up_lw, zsa) {
         return undefined;
     }
     const u0 = Math.cos(zsa * Math.PI/180.0);
-
+    if (u0 <= 0.0) {
+        return undefined;
+    }
     if (direct > 1500.0) {
         return undefined;
     }
-    if (diffuse > 1500 * 0.95 * u0 ** 1.2 + 50) {
+    if (diffuse > 1500 * 0.95 * Math.pow(u0, 1.2) + 50) {
         return undefined;
     }
     const down_sw = direct * u0 + diffuse;
     return (down_sw - up_sw) + (down_lw - up_lw);
 }
-function netshortwave(direct, diffuse, up_sw, down_lw, up_lw, zsa) {
+
+function netshortwave(direct, diffuse, up_sw, zsa) {
     if (!isFinite(direct) || !isFinite(diffuse) || !isFinite(up_sw) || !isFinite(zsa)) {
         return undefined;
     }
     const u0 = Math.cos(zsa * Math.PI/180.0);
-
+    if (u0 <= 0.0) {
+        return undefined;
+    }
     if (direct > 1500.0) {
         return undefined;
     }
-    if (diffuse > 1500 * 0.95 * u0 ** 1.2 + 50) {
+    if (diffuse > 1500 * 0.95 * Math.pow(u0, 1.2) + 50) {
         return undefined;
     }
     const down_sw = direct * u0 + diffuse;
     return down_sw - up_sw;
 }
-function netlongwave(direct, diffuse, up_sw, down_lw, up_lw, zsa) {
+
+function netlongwave(down_lw, up_lw) {
     if (!isFinite(down_lw) || !isFinite(up_lw)) {
         return undefined;
     }
     return down_lw - up_lw;
 }
-    return new GenericOperations.ApplyToFields(dataName, { net: net, netshortwave: netshortwave, netlongwave: netlongwave }, 'direct', 'diffuse', 'up_sw', 'down_lw', 'up_lw', 'zsa');
+
+    return new GenericOperations.ApplyToFields(dataName, {
+        'net': (output, direct, diffuse, up_sw, down_lw, up_lw, zsa) => net(direct, diffuse, up_sw, down_lw, up_lw, zsa),
+        'netshortwave': (output, direct, diffuse, up_sw, down_lw, up_lw, zsa) => netshortwave(direct, diffuse, up_sw, zsa),
+        'netlongwave': (output, direct, diffuse, up_sw, down_lw, up_lw, zsa) => netlongwave(down_lw, up_lw),
+    }, 'direct', 'diffuse', 'up_sw', 'down_lw', 'up_lw', 'zsa');
 })"""
 
     def __init__(
